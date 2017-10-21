b055d277a2663bce7da4a4b3295ba0e19f0cc586
Johan Vanderhaegen <4908616+johanvdhaegen@users.noreply.github.com>
2017-10-21 07:18:01 -0700

    Update chktex to version 1.7.6.
    GitOrigin-RevId: a7dea77b3c82b732179a54e68da46bec1fdab46b

--- Formula/chktex.rb
+++ Formula/chktex.rb
@@ -1,9 +1,9 @@
 class Chktex < Formula
   desc "LaTeX semantic checker"
   homepage "http://www.nongnu.org/chktex/"
-  url "https://download.savannah.gnu.org/releases/chktex/chktex-1.7.4.tar.gz"
-  mirror "https://mirrors.kernel.org/debian/pool/main/c/chktex/chktex_1.7.4.orig.tar.gz"
-  sha256 "77ed995eabe7088dacf53761933da23e6bf8be14d461f364bd06e090978bf6d2"
+  url "https://download.savannah.gnu.org/releases/chktex/chktex-1.7.6.tar.gz"
+  mirror "https://mirrors.kernel.org/debian/pool/main/c/chktex/chktex_1.7.6.orig.tar.gz"
+  sha256 "8ac0e5ca213b2012d44c28f9e4feb9783df44750eb0c30a237d81ff58ef34c8d"
 
   head do
     url "svn://svn.sv.gnu.org/chktex/trunk/chktex"

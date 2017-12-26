2240a415652a62d78b58f3bfb0c58af6397b3cdf
Johan Vanderhaegen <4908616+johanvdhaegen@users.noreply.github.com>
2017-12-26 06:26:28 -0800

    Update copybara to version 2017-12-12.
    GitOrigin-RevId: fccfe2399ac1c462fd5904a87ea1836cb8fd0a66

--- Formula/copybara.rb
+++ Formula/copybara.rb
@@ -2,8 +2,8 @@ class Copybara < Formula
   desc "Tool for transforming and moving code between repositories"
   homepage "https://github.com/google/copybara"
   url "https://github.com/google/copybara.git",
-      :revision => "d3fb5a6fcc06b5b11deca339897d28d63e59bd14"
-  version "2017-10-18"
+      :revision => "2a2bfa11f553db7f7860a31dbcd74ce30f32d437"
+  version "2017-12-12"
 
   depends_on "bazel" => :build
   depends_on :java => "1.8+"

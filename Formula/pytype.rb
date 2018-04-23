2332494a0e11295d3598678c3c6e6cb02bdb070a
Johan Vanderhaegen <4908616+johanvdhaegen@users.noreply.github.com>
2018-04-23 07:46:52 -0700

    Add python package six as a resource to pytype homebrew formula.
    GitOrigin-RevId: 791387b24a9327f94d2ba45e026fe74342d5d4e5

--- Formula/pytype.rb
+++ Formula/pytype.rb
@@ -16,6 +16,11 @@ class Pytype < Formula
     sha256 "592766c6303207a20efc445587778322d7f73b161bd994f227adaa341ba212ab"
   end
 
+  resource "six" do
+    url "https://files.pythonhosted.org/packages/16/d8/bc6316cf98419719bd59c91742194c111b6f2e85abac88e496adefaf7afe/six-1.11.0.tar.gz"
+    sha256 "70e8a77beed4562e7f14fe23a786b54f6296e34344c23bc42f07b15018ff98e9"
+  end
+
   def install
     virtualenv_install_with_resources
   end

d55b3a363cdb6e20586a4ed00135f34d35d1bd65
Johan Vanderhaegen <4908616+johanvdhaegen@users.noreply.github.com>
2018-09-23 15:06:07 -0700

    Update pytype to version 2018-09-19.
    GitOrigin-RevId: 0704dbdc8d5505996081235684759bb1f095e3e8

--- Formula/pytype.rb
+++ Formula/pytype.rb
@@ -3,17 +3,32 @@ class Pytype < Formula
 
   desc "Static analyzer for Python code"
   homepage "https://github.com/google/pytype/"
-  url "https://github.com/google/pytype.git",
-      :revision => "726f8424983e2968605595e971be05db3b1dd185"
-  version "2018-05-15"
+  url "https://files.pythonhosted.org/packages/2b/af/95a6f453824dbe9c321e65049b935c9819c3d9b83af3b60c3a168baf5fad/pytype-2018.9.19.tar.gz"
+  sha256 "3a828aeedc65b79e033e14a6dd0a29b33395dc9d73ec613503d8ea0591937fcf"
+  version "2018-09-19"
   head "https://github.com/google/pytype.git"
 
   depends_on "python@2" if MacOS.version <= :snow_leopard
   # depends on "libyaml" => :recommend  # TODO(johan)
 
+  resource "decorator" do
+    url "https://files.pythonhosted.org/packages/6f/24/15a229626c775aae5806312f6bf1e2a73785be3402c0acdec5dbddd8c11e/decorator-4.3.0.tar.gz"
+    sha256 "c39efa13fbdeb4506c476c9b3babf6a718da943dab7811c206005a4a956c080c"
+  end
+
+  resource "networkx" do
+    url "https://files.pythonhosted.org/packages/f3/f4/7e20ef40b118478191cec0b58c3192f822cace858c19505c7670961b76b2/networkx-2.2.zip"
+    sha256 "45e56f7ab6fe81652fb4bc9f44faddb0e9025f469f602df14e3b2551c2ea5c8b"
+  end
+
+  resource "importlab" do
+    url "https://files.pythonhosted.org/packages/5c/bd/5cc7b843eea5597dbe8a28a2176ac9200cd39e29dd3d3118728f96af0d80/importlab-0.3.0.tar.gz"
+    sha256 "e1f0534182b248c9cf791bb60132450a40bbc1ac49ffc7d0c7930dfc107d4a85"
+  end
+
   resource "pyyaml" do
-    url "https://pyyaml.org/download/pyyaml/PyYAML-3.12.tar.gz"
-    sha256 "592766c6303207a20efc445587778322d7f73b161bd994f227adaa341ba212ab"
+    url "https://pyyaml.org/download/pyyaml/PyYAML-3.13.tar.gz"
+    sha256 "3ef3092145e9b70e3ddd2c7ad59bdd0252a94dfe3949721633e41344de00a6bf"
   end
 
   resource "six" do

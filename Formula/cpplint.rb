fcacc71861eb6cf49ec54a918966bc8c58a368cc
Johan Vanderhaegen <4908616+johanvdhaegen@users.noreply.github.com>
2018-12-25 11:27:54 -0800

    Use python2 for cpplint.
    GitOrigin-RevId: af5482fe324e69413a3f94c9bcd5a5de58f2a1fa

--- Formula/cpplint.rb
+++ Formula/cpplint.rb
@@ -6,18 +6,18 @@ class Cpplint < Formula
   url "https://raw.githubusercontent.com/google/styleguide/43d512ba13014d31f96053c0cdf299775681bc73/cpplint/cpplint.py"
   version "2018-04-15"
 
-  depends_on "python"
+  depends_on "python@2"
 
   def install
-    pyver = Language::Python.major_minor_version "python3"
+    pyver = Language::Python.major_minor_version "python"
     ENV.prepend_create_path "PYTHONPATH",
                             libexec/"lib/python#{pyver}/site-packages"
-    virtualenv_create(libexec, "python3")
+    virtualenv_create(libexec, "python")
     (libexec/"bin").install "cpplint.py"
     (bin/"cpplint").write <<~EOS
       #!/bin/bash
       PYTHONPATH="#{ENV["PYTHONPATH"]}"
-      "#{libexec}/bin/python3" "#{libexec}/bin/cpplint.py" "$@"
+      "#{libexec}/bin/python" "#{libexec}/bin/cpplint.py" "$@"
     EOS
   end
 end

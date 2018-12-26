62f20ec9fbccb83659b8b5eae5894f323f058041
Johan Vanderhaegen <4908616+johanvdhaegen@users.noreply.github.com>
2018-12-25 20:13:48 -0800

    Update copybara to version 2018-12-20 and fix copybara build:
    - Use java 1.8 for copybara
    - Add bazel args to use java 1.8
    
    GitOrigin-RevId: 161cb27f7e9891afdba6d3c108cc45c8d63fc6cc

--- Formula/copybara.rb
+++ Formula/copybara.rb
@@ -2,22 +2,30 @@ class Copybara < Formula
   desc "Tool for transforming and moving code between repositories"
   homepage "https://github.com/google/copybara"
   url "https://github.com/google/copybara.git",
-      :revision => "75d4916562a4b3743b1fb0ffdc847f73684595bd"
-  version "2018-11-16"
+      :revision => "1f753d188319643ca4af0b107ab01304c7092ed7"
+  version "2018-12-20"
 
   depends_on "bazel" => :build
-  depends_on :java => "1.9+"
+  depends_on :java => "1.8"
 
   def install
-    system "bazel", "build", "//java/com/google/copybara:copybara_deploy.jar"
+    cmd = Language::Java.java_home_cmd("1.8")
+    ENV["JAVA_HOME"] = Utils.popen_read(cmd).chomp
+
+    system "bazel", "build",
+           "--host_java_toolchain=@bazel_tools//tools/jdk:toolchain_hostjdk8",
+           "--java_toolchain=@bazel_tools//tools/jdk:toolchain_hostjdk8",
+           "//java/com/google/copybara:copybara_deploy.jar"
     libexec.install "bazel-bin/java/com/google/copybara/copybara_deploy.jar"
     (bin/"copybara").write <<~EOS
       #!/bin/bash
+      export JAVA_HOME="$(#{cmd})"
       CLASSPATH="#{libexec}/copybara_deploy.jar:." exec java -jar #{libexec}/copybara_deploy.jar "$@"
     EOS
   end
 
   test do
-    assert_match(/Unknown version$/, pipe_output("#{bin}/copybara version 2>&1"))
+    assert_match(/Unknown version$/,
+                 pipe_output("#{bin}/copybara version 2>&1"))
   end
 end

9147ba16d46ad02dea722a0e8bce21063cbeb0d3
Johan Vanderhaegen <4908616+johanvdhaegen@users.noreply.github.com>
2019-11-30 16:46:35 +0100

    Update pygments to version 2.5.2.
    GitOrigin-RevId: 91ca2ea90a5ef3d08c80622cbefe4486451c470b

--- Formula/pygments.rb
+++ Formula/pygments.rb
@@ -3,8 +3,8 @@ class Pygments < Formula
 
   desc "Syntax highlighting package written in Python"
   homepage "http://pygments.org/"
-  url "https://files.pythonhosted.org/packages/7e/ae/26808275fc76bf2832deb10d3a3ed3107bc4de01b85dcccbe525f2cd6d1e/Pygments-2.4.2.tar.gz"
-  sha256 "881c4c157e45f30af185c1ffe8d549d48ac9127433f2c380c24b84572ad66297"
+  url "https://files.pythonhosted.org/packages/cb/9f/27d4844ac5bf158a33900dbad7985951e2910397998e85712da03ce125f0/Pygments-2.5.2.tar.gz"
+  sha256 "98c8aa5a9f778fcd1026a17361ddaf7330d1b7c62ae97c3bb0ae73e0b9b6b0fe"
 
   depends_on "python"
 
@@ -14,7 +14,7 @@ class Pygments < Formula
 
   test do
     assert_match(
-      "<div class=\"highlight\"><pre><span></span><span class=\"k\">print</span> <span class=\"s2\">&quot;Hello World&quot;</span>\n</pre></div>\n",
+      "<div class=\"highlight\"><pre><span></span><span class=\"nb\">print</span> <span class=\"s2\">&quot;Hello World&quot;</span>\n</pre></div>\n",
       shell_output(
         "echo 'print \"Hello World\"' | pygmentize -l python -f html",
       ),

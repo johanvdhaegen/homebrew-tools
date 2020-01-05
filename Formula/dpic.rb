e6e62bb06b9799bc5cf4db375af3982e167b389e
Johan Vanderhaegen <4908616+johanvdhaegen@users.noreply.github.com>
2020-01-05 10:24:24 -0800

    Add dpic homebrew formula.

---
+++ Formula/dpic.rb
@@ -0,0 +1,29 @@
+class Dpic < Formula
+  desc "Implementation of the pic language"
+  homepage "https://ece.uwaterloo.ca/~aplevich/dpic"
+  url "https://ece.uwaterloo.ca/~aplevich/dpic/dpic-2019.11.30.tar.gz"
+  sha256 "af0d47f29fc08d4fa3eac158d6a52ab6087b82160725f273aa0babd56a18ed65"
+
+  def install
+    ENV.append_path "PATH", "/Library/TeX/texbin"
+    system "make", "DESTDIR=/", "PREFIX=#{prefix}", "MANDIR=#{man1}", "DOCDIR=#{doc}", "install"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/dpic -h 2>&1", 1)
+    (testpath/"test.pic").write <<~EOS
+      .PS
+        arrow "$u$" above
+      S: circle rad 10/72.27  # 10 pt
+        line right 0.35
+      G: box "$G(s)$"
+        arrow "$y$" above
+        line down G.ht from last arrow then left last arrow.c.x-S.x
+        arrow to S.s
+        "$-\;$" below rjust
+      .PE
+    EOS
+    output = shell_output("#{bin}/dpic test.pic")
+    assert_match "\\begin{picture}", output
+  end
+end

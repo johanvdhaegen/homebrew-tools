23e60cb54f4f9305eb5587f6daac58c8205002b7
Johan Vanderhaegen <4908616+johanvdhaegen@users.noreply.github.com>
2020-01-05 10:01:31 -0800

    Lint fix for pygments homebrew formula.
    GitOrigin-RevId: e25d166d0dbb30c0cac93ab6550083125dddb4a7

--- Formula/pygments.rb
+++ Formula/pygments.rb
@@ -2,7 +2,7 @@ class Pygments < Formula
   include Language::Python::Virtualenv
 
   desc "Syntax highlighting package written in Python"
-  homepage "http://pygments.org/"
+  homepage "https://pygments.org/"
   url "https://files.pythonhosted.org/packages/cb/9f/27d4844ac5bf158a33900dbad7985951e2910397998e85712da03ce125f0/Pygments-2.5.2.tar.gz"
   sha256 "98c8aa5a9f778fcd1026a17361ddaf7330d1b7c62ae97c3bb0ae73e0b9b6b0fe"
 

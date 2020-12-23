a0ef3a3482a319587719523d8c297693cc0d01a7
Johan Vanderhaegen <4908616+johanvdhaegen@users.noreply.github.com>
2020-12-23 07:39:09 -0800

    magic: update to version 8.3.102
    - Depend on libx11 instead of on :x11.
    
    GitOrigin-RevId: 5cfb9c29be81289a918378ff17c7c96552a477a7

--- Formula/magic.rb
+++ Formula/magic.rb
@@ -1,8 +1,8 @@
 class Magic < Formula
   desc "VLSI layout tool"
   homepage "http://opencircuitdesign.com/magic"
-  url "http://opencircuitdesign.com/magic/archive/magic-8.3.32.tgz"
-  sha256 "4aff163ce617a235e2985fab5a0db6d7ec325bb7fc9f817af25a981ec1e34fa9"
+  url "http://opencircuitdesign.com/magic/archive/magic-8.3.102.tgz"
+  sha256 "6fa8c8686f35540155eed119d63a4f834eb781b922623195c9948e115d1e5828"
   license "ISC"
 
   livecheck do
@@ -12,9 +12,9 @@ class Magic < Formula
 
   depends_on "pkg-config" => :build
   depends_on "cairo-x11"
+  depends_on "libx11"
   depends_on "readline"
   depends_on "tcl-tk-x11"
-  depends_on :x11
 
   def install
     args = [

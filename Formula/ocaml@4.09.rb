cccb34c497c71a9bb3c51af655d8f21d83c6bbb6
Johan Vanderhaegen <4908616+johanvdhaegen@users.noreply.github.com>
2021-11-18 07:33:21 -0800

    ocaml@4.09: fix flat_namespace.
    GitOrigin-RevId: caf8f560729e32262a62712905ead70bfa9f8073

--- Formula/ocaml@4.09.rb
+++ Formula/ocaml@4.09.rb
@@ -15,6 +15,9 @@ class OcamlAT409 < Formula
 
   keg_only :versioned_formula
 
+  # Fix -flat_namespace
+  patch :DATA
+
   def install
     ENV.deparallelize # Builds are not parallel-safe, esp. with many cores
 
@@ -35,3 +38,38 @@ class OcamlAT409 < Formula
     assert_match HOMEBREW_PREFIX.to_s, shell_output("#{bin}/ocamlc -where")
   end
 end
+
+__END__
+diff -ur ocaml-4.09.0_orig/configure ocaml-4.09.0/configure
+--- a/configure	2019-09-19 00:23:44.000000000 -0700
++++ b/configure	2021-11-18 06:27:42.836955679 -0800
+@@ -7484,16 +7484,11 @@
+       _lt_dar_allow_undefined='$wl-undefined ${wl}suppress' ;;
+     darwin1.*)
+       _lt_dar_allow_undefined='$wl-flat_namespace $wl-undefined ${wl}suppress' ;;
+-    darwin*) # darwin 5.x on
+-      # if running on 10.5 or later, the deployment target defaults
+-      # to the OS version, if on x86, and 10.4, the deployment
+-      # target defaults to 10.4. Don't you love it?
+-      case ${MACOSX_DEPLOYMENT_TARGET-10.0},$host in
+-	10.0,*86*-darwin8*|10.0,*-darwin[91]*)
+-	  _lt_dar_allow_undefined='$wl-undefined ${wl}dynamic_lookup' ;;
+-	10.[012][,.]*)
++    darwin*)
++      case ${MACOSX_DEPLOYMENT_TARGET},$host in
++	10.[012],*|,*powerpc*)
+ 	  _lt_dar_allow_undefined='$wl-flat_namespace $wl-undefined ${wl}suppress' ;;
+-	10.*)
++	*)
+ 	  _lt_dar_allow_undefined='$wl-undefined ${wl}dynamic_lookup' ;;
+       esac
+     ;;
+@@ -13286,7 +13281,7 @@
+ if test x"$enable_shared" != "xno"; then :
+   case $host in #(
+   *-apple-darwin*) :
+-    mksharedlib="$CC -shared -flat_namespace -undefined suppress \
++    mksharedlib="$CC -shared -undefined dynamic_lookup \
+                    -Wl,-no_compact_unwind"
+       shared_libraries_supported=true ;; #(
+   *-*-mingw32) :

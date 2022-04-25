5b5003e74b63517bf21647bcc41359cb446e2487
Johan Vanderhaegen <4908616+johanvdhaegen@users.noreply.github.com>
2022-04-25 07:04:11 -0700

    Add emacs-cocoa@28 homebrew formula.
    GitOrigin-RevId: fc8c6138175fa32dcdc0ea98f241bd52168bc077

---
+++ Formula/emacs-cocoa@28.rb
@@ -0,0 +1,130 @@
+class EmacsCocoaAT28 < Formula
+  desc "GNU Emacs text editor"
+  homepage "https://www.gnu.org/software/emacs/"
+  url "https://github.com/emacs-mirror/emacs.git",
+      branch:   "emacs-28",
+      revision: "7c1fc2348918a1ad8c61c7519a51042d48d7aa8d"
+  version "28.1.20220425"
+  license "GPL-3.0-or-later"
+
+  keg_only :versioned_formula
+
+  option "with-ctags", "Don't remove the ctags executable that emacs provides"
+  option "without-modules", "Build without dynamic modules support"
+  option "without-xwidgets", "Build without Xwidgets support"
+
+  depends_on "autoconf" => :build
+  depends_on "gnu-sed" => :build
+  depends_on "pkg-config" => :build
+  depends_on "texinfo" => :build
+  depends_on "gnutls"
+  depends_on :macos
+  depends_on "jansson" => :recommended
+  depends_on "librsvg" => :recommended
+  depends_on "little-cms2" => :recommended
+  depends_on "dbus" => :optional
+  depends_on "imagemagick" => :optional
+  depends_on "mailutils" => :optional
+
+  uses_from_macos "libxml2"
+
+  def install
+    args = %W[
+      --disable-silent-rules
+      --enable-locallisppath=#{HOMEBREW_PREFIX}/share/emacs/site-lisp
+      --infodir=#{info}/emacs
+      --prefix=#{prefix}
+      --without-x
+      --with-ns
+      --disable-ns-self-contained
+      --with-gnutls
+      --with-xml2
+      --with-jpeg
+      --with-png
+    ]
+
+    args << "--with-rsvg#{build.with?("librsvg") ? "" : "=no"}"
+    args << "--with-lcms2#{build.with?("little-cms2") ? "" : "=no"}"
+    args << "--without-pop" if build.with? "mailutils"
+    args << "--with-imagemagick#{build.with?("imagemagick") ? "" : "=no"}"
+    args << "--with-json#{build.with?("jansson") ? "" : "=no"}"
+    args << "--with-dbus#{build.with?("dbus") ? "" : "=no"}"
+    args << "--with-modules#{build.with?("modules") ? "" : "=no"}"
+    args << "--with-xwidgets#{build.with?("xwidgets") ? "" : "=no"}"
+
+    ENV.prepend_path "PATH", Formula["gnu-sed"].opt_libexec/"gnubin"
+    system "./autogen.sh"
+
+    File.write "lisp/site-load.el", <<~EOS
+      (setq exec-path (delete nil
+        (mapcar
+          (lambda (elt)
+            (unless (string-match-p "Homebrew/shims" elt) elt))
+          exec-path)))
+    EOS
+
+    system "./configure", *args
+    system "make"
+    system "make", "install"
+
+    prefix.install "nextstep/Emacs.app"
+
+    # Replace the symlink with one that avoids starting Cocoa.
+    (bin/"emacs").unlink # Kill the existing symlink
+    (bin/"emacs").write <<~EOS
+      #!/bin/bash
+      exec #{prefix}/Emacs.app/Contents/MacOS/Emacs "$@"
+    EOS
+
+    # Remove default site-lisp subdirs.el file.
+    (share/"emacs/site-lisp/subdirs.el").unlink
+
+    # Follow MacPorts and don't install ctags from Emacs. This allows Vim
+    # and Emacs and ctags to play together without violence.
+    return if build.with? "ctags"
+
+    (bin/"ctags").unlink
+    (man1/"ctags.1.gz").unlink
+  end
+
+  def caveats
+    <<~EOS
+      Emacs.app has been installed in:
+        #{prefix}
+      To link the application to the default App location:
+        ln -s #{prefix}/Emacs.app /Applications
+    EOS
+  end
+
+  plist_options manual: "emacs"
+
+  def plist
+    <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+      <dict>
+        <key>KeepAlive</key>
+        <true/>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>ProgramArguments</key>
+        <array>
+          <string>#{opt_bin}/emacs</string>
+          <string>--fg-daemon</string>
+        </array>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>StandardOutPath</key>
+        <string>/tmp/emacs.stdout.log</string>
+        <key>StandardErrorPath</key>
+        <string>/tmp/emacs.stderr.log</string>
+      </dict>
+      </plist>
+    EOS
+  end
+
+  test do
+    assert_equal "4", shell_output("#{bin}/emacs --batch --eval=\"(print (+ 2 2))\"").strip
+  end
+end

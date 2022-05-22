1c09695de41209955e16d1df1472ebcfffb9c0bc
Johan Vanderhaegen <4908616+johanvdhaegen@users.noreply.github.com>
2022-05-22 15:01:46 -0700

    emacs-cocoa@28: add 28.1.20220521 bottle.
    GitOrigin-RevId: da5b263f2d4a12ed109d1daaa4b4815d7062675e

--- Formula/emacs-cocoa@28.rb
+++ Formula/emacs-cocoa@28.rb
@@ -7,6 +7,11 @@ class EmacsCocoaAT28 < Formula
   version "28.1.20220521"
   license "GPL-3.0-or-later"
 
+  bottle do
+    root_url "https://github.com/johanvdhaegen/homebrew-tools/releases/download/emacs-cocoa@28-28.1.20220521"
+    sha256 big_sur: "2af35e55b54d9e3b06d08a0a4c2328d0db93c16f104ba4d7da5630b433fe963e"
+  end
+
   keg_only :versioned_formula
 
   option "with-ctags", "Don't remove the ctags executable that emacs provides"

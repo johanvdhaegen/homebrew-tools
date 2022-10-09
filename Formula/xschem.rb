d2ff5236491bf01f6aebf5f62c468c56fcf83ddd
Johan Vanderhaegen <4908616+johanvdhaegen@users.noreply.github.com>
2022-10-09 11:14:31 -0700

    xschem: add 3.1.0 bottle.
    GitOrigin-RevId: 66c964afde1c04aa963067d04dbd53df7f334fa5

--- Formula/xschem.rb
+++ Formula/xschem.rb
@@ -10,6 +10,12 @@ class Xschem < Formula
     regex(/xschem-v?(\d+(?:\.\d+)+)\.t/i)
   end
 
+  bottle do
+    root_url "https://github.com/johanvdhaegen/homebrew-tools/releases/download/xschem-3.1.0"
+    sha256 big_sur:      "0071da012b217cf630cd8f25331e8793f4724cd65cc5577c754098c5313556fe"
+    sha256 x86_64_linux: "b9093934626d327b76ae59cf3a2f090a32ec7abd77fc96af2262beb3c3152d20"
+  end
+
   depends_on "pkg-config" => :build
   depends_on "cairo"
   depends_on "libx11"

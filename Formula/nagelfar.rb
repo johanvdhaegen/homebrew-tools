44c1b3e68e3a84b173b4e5dbd0e2a8c3404037ee
Johan Vanderhaegen <4908616+johanvdhaegen@users.noreply.github.com>
2022-10-29 17:31:01 -0700

    nagelfar: add 1.3.2 bottle.
    GitOrigin-RevId: 0bf2c352bcc6fab07cfafbaaa2dca85f7f8c6da3

--- Formula/nagelfar.rb
+++ Formula/nagelfar.rb
@@ -6,6 +6,12 @@ class Nagelfar < Formula
   sha256 "ff704e551efa2f39ba851ce9f02cc8ab66c7fae39fb41d5aa2abd6fe05c76b72"
   license "GPL-2.0-only"
 
+  bottle do
+    root_url "https://github.com/johanvdhaegen/homebrew-tools/releases/download/nagelfar-1.3.2"
+    sha256 cellar: :any_skip_relocation, big_sur:      "4765101085c948e77df71cdc0aae61081b8011fea61b1e612600a70d30825952"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "2dfd0e4ae1a4b4ae34c6650651aa20a05c8c34038aad04d6a4b5bebdf70b6fe7"
+  end
+
   depends_on "tcl-tk"
 
   def install

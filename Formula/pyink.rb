905c8bf2348268084dcccfabaad064e79e635859
Johan Vanderhaegen <4908616+johanvdhaegen@users.noreply.github.com>
2022-10-30 08:19:08 -0700

    pyink: add pyink python code formatter homebrew formula.
    GitOrigin-RevId: 709473f766fa9ab6d054884ddbe3445698cfc2d5

---
+++ Formula/pyink.rb
@@ -0,0 +1,57 @@
+class Pyink < Formula
+  include Language::Python::Virtualenv
+
+  desc "Python code formatter"
+  homepage "https://github.com/google/pyink"
+
+  url "https://files.pythonhosted.org/packages/81/60/3b391a128f6939d8681cc22d60f7a7c5f23ffb0e516094cd9ae6bf9e067e/pyink-22.10.0.tar.gz"
+  sha256 "b3dd1ad9ae37c187e927b2f48df769af58c187c6589bbd7aba3e2567884cb831"
+  license "MIT"
+  head "https://github.com/google/pyink.git", branch: "pyink"
+
+  depends_on "python@3.10"
+
+  resource "click" do
+    url "https://files.pythonhosted.org/packages/59/87/84326af34517fca8c58418d148f2403df25303e02736832403587318e9e8/click-8.1.3.tar.gz"
+    sha256 "7682dc8afb30297001674575ea00d1814d808d6a36af415a82bd481d37ba7b8e"
+  end
+
+  resource "mypy-extensions" do
+    url "https://files.pythonhosted.org/packages/63/60/0582ce2eaced55f65a4406fc97beba256de4b7a95a0034c6576458c6519f/mypy_extensions-0.4.3.tar.gz"
+    sha256 "2d82818f5bb3e369420cb3c4060a7970edba416647068eb4c5343488a6c604a8"
+  end
+
+  resource "pathspec" do
+    url "https://files.pythonhosted.org/packages/24/9f/a9ae1e6efa11992dba2c4727d94602bd2f6ee5f0dedc29ee2d5d572c20f7/pathspec-0.10.1.tar.gz"
+    sha256 "7ace6161b621d31e7902eb6b5ae148d12cfd23f4a249b9ffb6b9fee12084323d"
+  end
+
+  resource "platformdirs" do
+    url "https://files.pythonhosted.org/packages/ff/7b/3613df51e6afbf2306fc2465671c03390229b55e3ef3ab9dd3f846a53be6/platformdirs-2.5.2.tar.gz"
+    sha256 "58c8abb07dcb441e6ee4b11d8df0ac856038f944ab98b7be6b27b2a3c7feef19"
+  end
+
+  resource "tomli" do
+    url "https://files.pythonhosted.org/packages/c0/3f/d7af728f075fb08564c5949a9c95e44352e23dee646869fa104a3b2060a3/tomli-2.0.1.tar.gz"
+    sha256 "de526c12914f0c550d15924c62d72abc48d6fe7364aa87328337a31007fe8a4f"
+  end
+
+  resource "black" do
+    url "https://files.pythonhosted.org/packages/a3/89/629fca2eea0899c06befaa58dc0f49d56807d454202bb2e54bd0d98c77f3/black-22.10.0.tar.gz"
+    sha256 "f513588da599943e0cde4e32cc9879e825d58720d6557062d1098c5ad80080e1"
+  end
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  test do
+    ENV["LC_ALL"] = "en_US.UTF-8"
+    (testpath/"pyink_test.py").write <<~EOS
+      print(
+      'It works!')
+    EOS
+    system bin/"pyink", "pyink_test.py"
+    assert_equal "print(\"It works!\")\n", (testpath/"pyink_test.py").read
+  end
+end

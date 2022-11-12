1a4ed3faec34472542ef72886e26a44319f2df4e
Johan Vanderhaegen <4908616+johanvdhaegen@users.noreply.github.com>
2022-11-11 21:01:13 -0800

    yamlfmt: update to version 0.6.
    GitOrigin-RevId: f5cfced54adc2c02c82b561f249d247a156a9cfe

--- Formula/yamlfmt.rb
+++ Formula/yamlfmt.rb
@@ -2,15 +2,9 @@ class Yamlfmt < Formula
   desc "Extensible command-line tool or library to format yaml files"
   homepage "https://github.com/google/yamlfmt"
   url "https://github.com/google/yamlfmt.git",
-      tag:      "v0.5.0",
-      revision: "f0be560f35be8966cbcc84451d11f025973df8a6"
+      tag:      "v0.6.0",
+      revision: "811c65058125f094d4d5d886253f9a3c92b5a81d"
   license "Apache-2.0"
-
-  bottle do
-    root_url "https://github.com/johanvdhaegen/homebrew-tools/releases/download/yamlfmt-0.5.0"
-    sha256 cellar: :any_skip_relocation, big_sur:      "757d32d87026e6aa74a1eb1f8d838d012b81f9383415facb56c2153121c9f6ab"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "ebb6babf730e703f43fd60ad849a8b06aa3dd8652d803973d6d2535cb676007e"
-  end
 
   head do
     url "https://github.com/google/yamlfmt.git", branch: "main"

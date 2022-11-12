f43f8f3aae6653a540c54d0c7d507b1ce173de41
Johan Vanderhaegen <4908616+johanvdhaegen@users.noreply.github.com>
2022-11-11 22:30:49 -0800

    yamlfmt: add 0.6.0 bottle.
    GitOrigin-RevId: e5f6ba6141f7bac98c4cdc9a06178ed2e1ec908c

--- Formula/yamlfmt.rb
+++ Formula/yamlfmt.rb
@@ -6,6 +6,12 @@ class Yamlfmt < Formula
       revision: "811c65058125f094d4d5d886253f9a3c92b5a81d"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://github.com/johanvdhaegen/homebrew-tools/releases/download/yamlfmt-0.6.0"
+    sha256 cellar: :any_skip_relocation, big_sur:      "2519dbfa26e22d8e5817b19d1b8031b71149f0877cea1bf8f7fea8cb91298498"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "79a07960fd4cd8858093cf30c38734725a0902158aa9762a0b8258f0c54ae4d6"
+  end
+
   head do
     url "https://github.com/google/yamlfmt.git", branch: "main"
   end

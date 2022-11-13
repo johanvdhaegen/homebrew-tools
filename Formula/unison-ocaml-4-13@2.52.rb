9c6f08d22becbcf1ca7f4e1643825b2a93a4cba0
Johan Vanderhaegen <4908616+johanvdhaegen@users.noreply.github.com>
2022-11-12 16:17:06 -0800

    unison-ocaml-4-13@2.52: use lablgtk-ocaml-4-13@2 versioned formula.
    GitOrigin-RevId: 05ee5f61c03a7daccb36995703a8f88469b13b56

--- Formula/unison-ocaml-4-13@2.52.rb
+++ Formula/unison-ocaml-4-13@2.52.rb
@@ -13,17 +13,11 @@ class UnisonOcaml413AT252 < Formula
     regex(/^v?(2\.52\.[0-5]\d*)$/i)
   end
 
-  bottle do
-    root_url "https://github.com/johanvdhaegen/homebrew-tools/releases/download/unison-ocaml-4-13@2.52-2.52.1"
-    sha256 cellar: :any_skip_relocation, big_sur:      "8dccb8355b2212c5787d6e14429dd7a283fc3a27afd6ff753d47d99135cb3146"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "3b2d89d5bc61cae9521a2b430ef24822e540f9df894d4db4e5b24a0f5ccdb07f"
-  end
-
   keg_only :versioned_formula
 
   depends_on "ocaml@4.13" => :build
   depends_on "gtk+" => OS.mac? ? :recommended : :optional
-  depends_on "lablgtk-ocaml-4-13" => :build if build.with? "gtk+"
+  depends_on "lablgtk-ocaml-4-13@2" => :build if build.with? "gtk+"
 
   def install
     ENV.deparallelize

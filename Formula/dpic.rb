296365b8af25e9f18111c6511e1629da0b0589a4
Johan Vanderhaegen <4908616+johanvdhaegen@users.noreply.github.com>
2023-02-03 17:57:52 -0800

    dpic: update to version 2023.02.01.
    GitOrigin-RevId: e8f3c6b3d30f25fd62e86c9750e0a7f1002a6a6f

--- Formula/dpic.rb
+++ Formula/dpic.rb
@@ -1,20 +1,13 @@
 class Dpic < Formula
   desc "Implementation of the pic language"
   homepage "https://ece.uwaterloo.ca/~aplevich/dpic"
-  url "https://ece.uwaterloo.ca/~aplevich/dpic/dpic-2022.12.01.tar.gz"
-  sha256 "041b9e13bb5c61707c3d5cb77b2686fbf679c4de8a8d048897b0c6655058ee14"
+  url "https://ece.uwaterloo.ca/~aplevich/dpic/dpic-2023.02.01.tar.gz"
+  sha256 "d059ff28c04552066c164fb146fee8e0101b953e46e75919b3dcfaa99b031ae6"
   license "BSD-2-Clause"
-  revision 1
 
   livecheck do
     url :homepage
     regex(/href=.*dpic[._-]v?(\d+(?:\.\d+)+)\.t/i)
-  end
-
-  bottle do
-    root_url "https://github.com/johanvdhaegen/homebrew-tools/releases/download/dpic-2022.12.01_1"
-    sha256 cellar: :any_skip_relocation, monterey:     "de9ef30da2fdb61cbff93fc6fcb40a190e9489ce7d47b4f45fa2dc9ccf2b5f35"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "405d101c285e6ea3a29155375bef70294adf4156aca6e17d26d75bc2313cc6da"
   end
 
   def install

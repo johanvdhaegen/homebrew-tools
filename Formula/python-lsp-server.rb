dfc4532440a38459d89e79a34db27c9abc74bca6
Johan Vanderhaegen <4908616+johanvdhaegen@users.noreply.github.com>
2023-03-05 07:55:34 -0800

    python-lsp-server: bump revision.
    GitOrigin-RevId: c5939aef1ed69d6e04fefb2568e20ef8d540846f

--- Formula/python-lsp-server.rb
+++ Formula/python-lsp-server.rb
@@ -6,7 +6,7 @@ class PythonLspServer < Formula
   url "https://files.pythonhosted.org/packages/59/c5/fdb678be01f90450cb3c785eba8fbdd80361156c31da96137eb5c31f9a66/python-lsp-server-1.7.1.tar.gz"
   sha256 "67473bb301f35434b5fa8b21fc5ed5fac27dc8a8446ccec8bae456af52a0aef6"
   license "MIT"
-  revision 1
+  revision 2
 
   bottle do
     root_url "https://github.com/johanvdhaegen/homebrew-tools/releases/download/python-lsp-server-1.7.1_1"

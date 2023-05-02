c2f32db8ce3a9476cf70e7142425741749518dac
Johan Vanderhaegen <4908616+johanvdhaegen@users.noreply.github.com>
2023-05-02 12:17:17 -0700

    emacs-cocoa@29: update to version 29.0.20230502.
    GitOrigin-RevId: 29974cf0840e87739b875934f917a47442d3802e

--- Formula/emacs-cocoa@29.rb
+++ Formula/emacs-cocoa@29.rb
@@ -3,8 +3,8 @@ class EmacsCocoaAT29 < Formula
   homepage "https://www.gnu.org/software/emacs/"
   url "https://github.com/emacs-mirror/emacs.git",
       branch:   "emacs-29",
-      revision: "ddfa0d8da9aafc926bd403c29a2cebfea571ebde"
-  version "29.0.20230428"
+      revision: "b93eb68cc30241059a339fbc697677f8f1d400ab"
+  version "29.0.20230502"
   license "GPL-3.0-or-later"
 
   bottle do

c2f426c284f50d57c95b862025d1167095a3bee1
Johan Vanderhaegen <4908616+johanvdhaegen@users.noreply.github.com>
2023-05-17 21:51:24 -0700

    wavedrom-cli: add 3.1.1 bottle.
    GitOrigin-RevId: 3b43c814eb86e7bf3d22d416d8e8db37ae055dc5

--- Formula/wavedrom-cli.rb
+++ Formula/wavedrom-cli.rb
@@ -7,6 +7,13 @@ class WavedromCli < Formula
   sha256 "7f627331241cb5560164885fc48db4ae7d1ccb8cea8f12ca00e4eb3ae07e21fa"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/johanvdhaegen/homebrew-tools/releases/download/wavedrom-cli-3.1.1"
+    sha256 cellar: :any,                 ventura:      "75885af028868376986ca37d00232ee380824a8518db238015716e6c9fabd38b"
+    sha256 cellar: :any,                 monterey:     "56f9778808929a9bf589c1479eeb6a67a14bf9f478184b9e033785a01517fc2a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "0ad5aedcc4878285612a274d505124c2928d067e836345b85161b2448e57e2f2"
+  end
+
   depends_on "node"
 
   def install

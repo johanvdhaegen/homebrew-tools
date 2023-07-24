d0ad9f2ff417cf6e1f99ed32b7eb8cc6e8767030
Johan Vanderhaegen <4908616+johanvdhaegen@users.noreply.github.com>
2023-07-24 11:05:42 -0700

    emacs-cocoa@29: update 29.1.20230723 bottle.
    GitOrigin-RevId: afd4aa37d89c58a4f9a402e55d35a1fd91c33656

--- Formula/emacs-cocoa@29.rb
+++ Formula/emacs-cocoa@29.rb
@@ -8,9 +8,9 @@ class EmacsCocoaAT29 < Formula
   license "GPL-3.0-or-later"
 
   bottle do
-    root_url "https://github.com/johanvdhaegen/homebrew-tools/releases/download/emacs-cocoa@29-29.0.20230705"
-    sha256 ventura:  "3bb44da5738314257e2d881ccebcc48c9c19d184d81e8d499b764ea2ffe697a6"
-    sha256 monterey: "b572180cd9e44a664a15c1d982a28b36d6c9ab9f8d507d4cdcb7402252b233e5"
+    root_url "https://github.com/johanvdhaegen/homebrew-tools/releases/download/emacs-cocoa@29-29.1.20230723"
+    sha256 ventura:  "2e3a3613402dcb11be206f8102f57ea734349b4f504b10e242ec92b775adedd4"
+    sha256 monterey: "bb38f4003072df6e90a7ae80eeadf961cf1c7d305be9452ca46044c7c82626fe"
   end
 
   keg_only :versioned_formula

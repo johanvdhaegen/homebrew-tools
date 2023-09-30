644680b1ad1b65935be9ca8f8d34707ec399f7d1
Johan Vanderhaegen <4908616+johanvdhaegen@users.noreply.github.com>
2023-09-30 13:53:41 -0700

    xschem: use github to download releases.
    GitOrigin-RevId: 414350f43fd7cd11e814cd81ae16178f0d00530d

--- Formula/xschem.rb
+++ Formula/xschem.rb
@@ -1,14 +1,9 @@
 class Xschem < Formula
   desc "Schematic editor for VLSI/Asic/Analog custom designs"
   homepage "https://github.com/StefanSchippers/xschem"
-  url "http://repo.hu/projects/xschem/releases/xschem-3.1.0.tar.bz2"
-  sha256 "dfa4002493de680b54881c3de3609d19420442fed0c2fef5f7c3f81f5bdf9013"
+  url "https://github.com/StefanSchippers/xschem/archive/refs/tags/3.1.0.tar.gz"
+  sha256 "04dea808e258a0b1622459d146fac0cc0aa875b98e729f2749a2d532b1114960"
   license "GPL-2.0-or-later"
-
-  livecheck do
-    url "http://repo.hu/projects/xschem/releases/"
-    regex(/xschem-v?(\d+(?:\.\d+)+)\.t/i)
-  end
 
   bottle do
     root_url "https://github.com/johanvdhaegen/homebrew-tools/releases/download/xschem-3.1.0"
@@ -20,6 +15,7 @@ class Xschem < Formula
   depends_on "cairo"
   depends_on "libx11"
   depends_on "libxpm"
+  depends_on :macos
   depends_on "tcl-tk-x11"
 
   def install

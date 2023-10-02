6f1f796481b246f1c9e4621a8269c7362938a93c
Johan Vanderhaegen <4908616+johanvdhaegen@users.noreply.github.com>
2023-10-01 17:16:50 -0700

    xschem: update 3.4.3 bottle.
    GitOrigin-RevId: 234d717dc496d53622284651d5c952d20ad6d471

--- Formula/xschem.rb
+++ Formula/xschem.rb
@@ -6,9 +6,9 @@ class Xschem < Formula
   license "GPL-2.0-or-later"
 
   bottle do
-    root_url "https://github.com/johanvdhaegen/homebrew-tools/releases/download/xschem-3.1.0"
-    sha256 big_sur:      "0071da012b217cf630cd8f25331e8793f4724cd65cc5577c754098c5313556fe"
-    sha256 x86_64_linux: "b9093934626d327b76ae59cf3a2f090a32ec7abd77fc96af2262beb3c3152d20"
+    root_url "https://github.com/johanvdhaegen/homebrew-tools/releases/download/xschem-3.4.3"
+    sha256 ventura:  "909ae84c0c7e8088a246d65eaa92c0b24e2bb8cc77b4bf19bd533b58ad2c2c93"
+    sha256 monterey: "c2609f5c5c10c812415b173ae35d2cdc2bbc369e70a5a9c59d21d01f75e4d254"
   end
 
   depends_on "pkg-config" => :build

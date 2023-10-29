ae60c1bfe809247d98970576d90ba509c0ddb332
Johan Vanderhaegen <4908616+johanvdhaegen@users.noreply.github.com>
2023-10-29 10:16:50 -0700

    ngspice: add customized ngspice homebrew formula.
    GitOrigin-RevId: fbbd8a76572dfc4eb9f78f8e0a4a5ba30daad9d7

---
+++ Formula/ngspice.rb
@@ -0,0 +1,59 @@
+class Ngspice < Formula
+  desc "Spice circuit simulator"
+  homepage "https://ngspice.sourceforge.io/"
+  url "https://downloads.sourceforge.net/project/ngspice/ng-spice-rework/41/ngspice-41.tar.gz"
+  sha256 "1ce219395d2f50c33eb223a1403f8318b168f1e6d1015a7db9dbf439408de8c4"
+  license :cannot_represent
+
+  livecheck do
+    url :stable
+    regex(%r{url=.*?/ngspice[._-]v?(\d+(?:\.\d+)*)\.t}i)
+  end
+
+  head do
+    url "https://git.code.sf.net/p/ngspice/ngspice.git", branch: "master"
+  end
+
+  keg_only "conflicts with ngspice"
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "bison" => :build
+  depends_on "libtool" => :build
+  depends_on "fftw"
+  depends_on "readline"
+  depends_on "tcl-tk"
+
+  uses_from_macos "bison" => :build
+
+  def install
+    system "./autogen.sh"
+
+    args = %w[
+      --with-readline=yes
+      --enable-xspice
+      --enable-cider
+      --enable-pss
+      --without-x
+    ]
+
+    system "./configure", *std_configure_args, *args
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.cir").write <<~EOS
+      RC test circuit
+      v1 1 0 1
+      r1 1 2 1
+      c1 2 0 1 ic=0
+      .tran 100u 100m uic
+      .control
+      run
+      quit
+      .endc
+      .end
+    EOS
+    system "#{bin}/ngspice", "test.cir"
+  end
+end

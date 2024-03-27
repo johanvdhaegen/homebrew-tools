a83a8a4d06fc7cefcb60222868d3935fba8bbcf5
Johan Vanderhaegen <4908616+johanvdhaegen@users.noreply.github.com>
2024-03-26 23:26:56 -0700

    emacs-cocoa@29: update 29.3.20240326 bottle.
    GitOrigin-RevId: d6553110700dff8606b42e689a18b1b51f8b0794

--- Formula/emacs-cocoa@29.rb
+++ Formula/emacs-cocoa@29.rb
@@ -9,7 +9,7 @@ class EmacsCocoaAT29 < Formula
 
   bottle do
     root_url "https://github.com/johanvdhaegen/homebrew-tools/releases/download/emacs-cocoa@29-29.3.20240326"
-    sha256 arm64_sonoma: "91ca48d3624bd3e33fedf99f7fee1c556634ab8543caadecffb781ed99931dbc"
+    sha256 arm64_sonoma: "bb3b15f6dfb5332568568e3e5eaf9e27cf1f831abea2b4f5982c633eb341d268"
     sha256 ventura:      "d90583dc9c2b0f2958df768468dc0ca6a2a2c0b57aa12acccec4b6a0a0cf3171"
   end
 

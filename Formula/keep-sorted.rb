50f0b1818afd407676a6e0f778c0718b7098b0c7
Johan Vanderhaegen <4908616+johanvdhaegen@users.noreply.github.com>
2025-01-22 22:59:39 -0800

    keep-sorted: update 0.6.0 bottle.
    GitOrigin-RevId: 86a557ff1987b25a6bacf3d82102230c172c337c

--- Formula/keep-sorted.rb
+++ Formula/keep-sorted.rb
@@ -6,10 +6,11 @@ class KeepSorted < Formula
   license "Apache-2.0"
 
   bottle do
-    root_url "https://github.com/johanvdhaegen/homebrew-tools/releases/download/keep-sorted-0.5.1"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma: "f2eb9f84ba0462d344ddb4bf526688999b8b489d253eb50b22bc1521357b8974"
-    sha256 cellar: :any_skip_relocation, ventura:      "2f5e63c54fdd09d6cfc5560742a255a5e4cb3088923c8658be0b2cecc6a06f21"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "1235ae1b2dde9c3fe31dd0ef42b9195e1087c271aecdcea8ff9d17be01a9e262"
+    root_url "https://github.com/johanvdhaegen/homebrew-tools/releases/download/keep-sorted-0.6.0"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "126f83ac460d98190241cd31b0df4b2399d5ffcde0e10ac96abfdf851b9a4551"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "843c571f1b6e4283604981d543eb373a513c11f51d8b5c1dd8a974851c9b04da"
+    sha256 cellar: :any_skip_relocation, ventura:       "fbeeb4db3f69cc6bb179d902bc5c897a624d377148895158665af6a4d16d3f23"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "aa08dcef714ecdd91efbdd5766f9297b5c00f81ac9304bd2dd3013251f71bb5b"
   end
 
   depends_on "go" => :build

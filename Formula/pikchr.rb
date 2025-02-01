bb1908ff91b2c6899ddd477eb0b37831e977a6cf
Johan Vanderhaegen <4908616+johanvdhaegen@users.noreply.github.com>
2025-02-01 14:23:45 -0800

    pikchr: add 1.0.0 bottle.
    GitOrigin-RevId: b8b421749fb2b9fb77b4f6eb1d1e6fc4b9ed8356

--- Formula/pikchr.rb
+++ Formula/pikchr.rb
@@ -6,6 +6,14 @@ class Pikchr < Formula
   license "0BSD"
   head "https://github.com/drhsqlite/pikchr.git", branch: "master"
 
+  bottle do
+    root_url "https://github.com/johanvdhaegen/homebrew-tools/releases/download/pikchr-1.0.0"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "93866302c68a748b1fc734c1381d44f3c42f18fb3bbd67a51a28b304b0bb1d0a"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "bd07147ad75c68cdf46f5832d9ae59350dc2afcad221013b0224c2e6e1efafb5"
+    sha256 cellar: :any_skip_relocation, ventura:       "e22779c869b70155eb251da74e103140280d53811d9cc11dd7ac5b516caec9a7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e269d9b87d46bc9957a09995f139b3e86a440b7127566e60b9b0453308bd5992"
+  end
+
   def install
     system "make", "all", "CC=#{ENV.cc}"
     bin.install "pikchr"

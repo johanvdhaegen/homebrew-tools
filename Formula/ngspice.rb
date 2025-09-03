fd41f51a6ac4fdd1cb1579e294203d860b04ef23
Johan Vanderhaegen <4908616+johanvdhaegen@users.noreply.github.com>
2025-09-02 22:22:43 -0700

    ngspice: update to version 45.
    GitOrigin-RevId: 13df04ba18968480d6236866dbb78515df244d9b

--- Formula/ngspice.rb
+++ Formula/ngspice.rb
@@ -1,8 +1,8 @@
 class Ngspice < Formula
   desc "Spice circuit simulator"
   homepage "https://ngspice.sourceforge.io/"
-  url "https://downloads.sourceforge.net/project/ngspice/ng-spice-rework/44.2/ngspice-44.2.tar.gz"
-  sha256 "e7dadfb7bd5474fd22409c1e5a67acdec19f77e597df68e17c5549bc1390d7fd"
+  url "https://downloads.sourceforge.net/project/ngspice/ng-spice-rework/45/ngspice-45.tar.gz"
+  sha256 "f1aad8abac2828a7b71da66411de8e406524e75f3066e46755439c490442d734"
   license :cannot_represent
 
   head "https://git.code.sf.net/p/ngspice/ngspice.git", branch: "master"

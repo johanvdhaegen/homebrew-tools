6d0276aac7e90caeb998921f24796e5130d4e180
Johan Vanderhaegen <4908616+johanvdhaegen@users.noreply.github.com>
2026-08-23 21:38:57 -0700

    trilinos-xyce: add 14.4.0 bottle.
    GitOrigin-RevId: b7430d5ce968949b71b9b1a9d08165a7f9dbfc7d

--- Formula/trilinos-xyce.rb
+++ Formula/trilinos-xyce.rb
@@ -13,6 +13,14 @@ class TrilinosXyce < Formula
     regex(/trilinos[._-]release[._-]v?(\d+)[._-](\d+)[._-](\d+)/i)
   end
 
+  bottle do
+    root_url "https://github.com/johanvdhaegen/homebrew-tools/releases/download/trilinos-xyce-14.4.0"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "1a2c0f9027aef82008cadd8cce3d7100a05a7f955c8fca250b27325f3ee7c87e"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "77f920f86ba2a50764b5ac1f59c4e0da235bc0ac784a229a4400e27eee4b7ffb"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "e3cc08f853d884a6695301f457aec8fd844c2caf592eceb85009f5b7eb101e57"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2b849ff1b6afa4ceb2aa342c15dff7ddcee3103772f8e18ec017f8b93bb76407"
+  end
+
   keg_only "it is built with only the subset of Trilinos that Xyce uses"
 
   depends_on "cmake" => :build
